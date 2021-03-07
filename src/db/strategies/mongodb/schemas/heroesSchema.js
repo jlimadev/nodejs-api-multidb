e3b9b27354c55f9ce63c7636914c2223cc9d60c8
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-07 17:10:49 -0300

    feat(mongo): add schema

---
+++ src/db/strategies/mongodb/schemas/heroesSchema.js
@@ -0,0 +1,20 @@
+const Mongoose = require('mongoose');
+const { v4 } = require('uuid');
+
+const modelName = 'heroes';
+
+const heroesSchema = new Mongoose.Schema({
+  _id: { type: String, required: true, default: v4 },
+  name: { type: String, required: true },
+  power: { type: String, required: true },
+  createdAt: { type: Date, default: new Date() },
+});
+
+const returnValue = Mongoose.models[modelName]
+  ? Mongoose.model(modelName)
+  : Mongoose.model(modelName, heroesSchema);
+
+console.log('Mongoose.model', Mongoose.model(modelName, heroesSchema));
+console.log('Mongoose.models[modelName]', Mongoose.models[modelName]);
+
+module.exports = returnValue;
