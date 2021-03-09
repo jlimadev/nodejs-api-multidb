2c5ccccce041b98a5a4dcdfeea84010526d11b56
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-09 07:53:11 -0300

    feat(defineEnviroment): create a helper to define env

---
+++ src/utils/defineEnvironment/index.js
@@ -0,0 +1,22 @@
+const { config } = require('dotenv');
+const { join } = require('path');
+
+const defineEnvironment = (env) => {
+  const checkEnvironment = /^devl$|prod$/;
+  if (!checkEnvironment.test(env)) {
+    throw new Error(
+      `[INVALID ENVIRONMENT] - ${env} is invalid. Must be "prod" or "devl"`,
+    );
+  }
+
+  const configPath = join(__dirname, '../../config', `.env.${env}`);
+  config({
+    path: configPath,
+  });
+
+  const message = `Successfuly set the environment to ${env}`;
+
+  return message;
+};
+
+module.exports = defineEnvironment;
---
+++ src/utils/defineEnvironment/index.spec.js
@@ -0,0 +1,44 @@
+const defineEnvironment = require('.');
+
+describe('defineEnvironment test suit', () => {
+  describe('Invalid environment cases', () => {
+    it('Should throw an error if the environment is undefined', () => {
+      const expectedErrorMessage = `[INVALID ENVIRONMENT] - undefined is invalid. Must be "prod" or "devl"`;
+
+      try {
+        defineEnvironment();
+      } catch (error) {
+        expect(error.message).toEqual(expectedErrorMessage);
+      }
+    });
+
+    it('Should throw an error if the environment is invalid', () => {
+      const env = 'anyOther';
+      const expectedErrorMessage = `[INVALID ENVIRONMENT] - ${env} is invalid. Must be "prod" or "devl"`;
+
+      try {
+        defineEnvironment(env);
+      } catch (error) {
+        expect(error.message).toEqual(expectedErrorMessage);
+      }
+    });
+  });
+
+  describe('Valid environments', () => {
+    it('Should set the environment to devl', () => {
+      const env = 'devl';
+      const expectedSuccessMessage = `Successfuly set the environment to ${env}`;
+      const result = defineEnvironment(env);
+
+      expect(result).toEqual(expectedSuccessMessage);
+    });
+
+    it('Should set the environment to prod', () => {
+      const env = 'prod';
+      const expectedSuccessMessage = `Successfuly set the environment to ${env}`;
+      const result = defineEnvironment(env);
+
+      expect(result).toEqual(expectedSuccessMessage);
+    });
+  });
+});
