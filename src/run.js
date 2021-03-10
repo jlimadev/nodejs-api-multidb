e9d46f431906b8f4c87960b5f99045f81a9d3998
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-10 17:08:16 -0300

    chore(remove hapijs): to refactor

--- src/run.js
+++
@@ -1,6 +0,0 @@
-const env = process.env.NODE_ENV || 'devl';
-const defineEnvironment = require('./utils/defineEnvironment');
-defineEnvironment(env);
-
-const { handler } = require('./index');
-handler();
