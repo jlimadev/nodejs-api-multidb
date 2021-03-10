5c8327cfa3ee03c17079e1c4b68183c43c3de6bc
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-10 11:33:57 -0300

    feat(sever): adjust deps and tests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 const Hapi = require('@hapi/hapi');
+const ContextStrategy = require('./db/strategies/context/ContextStrategy');
+const Mongo = require('./db/strategies/mongodb/mongodb');
+const MongoHeroesSchema = require('./db/strategies/mongodb/schemas/heroesSchema');
+const Postgres = require('./db/strategies/postgres/postgres');
+const PostgresHeroesSchema = require('./db/strategies/postgres/schemas/HeroesSchema');
 
 const app = new Hapi.Server({
   port: process.env.PORT,
 });
 
+const defineDatabase = async () => {
+  const mongoConnection = Mongo.connect();
+  const mongoInstance = new Mongo(mongoConnection, MongoHeroesSchema);
+  const mongoStrategy = new ContextStrategy(mongoInstance);
+
+  const postgresConnection = await Postgres.connect();
+  const postgresModel = await Postgres.defineModel(
+    postgresConnection,
+    PostgresHeroesSchema,
+  );
+  const postgresInstance = new Postgres(postgresConnection, postgresModel);
+  const postgresStrategy = new ContextStrategy(postgresInstance);
+
+  const databases = {
+    mongo: {
+      Mongo,
+      mongoStrategy,
+    },
+    postgres: {
+      Postgres,
+      postgresConnection,
+      postgresStrategy,
+    },
+  };
+
+  return databases;
+};
+
 const deps = {
   app,
+  defineDatabase,
 };
 
 module.exports.handler = require('./core')(deps);
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,12 @@
 const { handler, deps } = require('.');
+const ContextStrategy = require('./db/strategies/context/ContextStrategy');
 
-describe.only('application handler', () => {
+describe('application handler', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+    jest.setTimeout(10000);
+  });
+
   it('Should return a promise', () => {
     expect(handler).toBeInstanceOf(Object);
   });
@@ -8,5 +14,18 @@ describe.only('application handler', () => {
   it('Should return an object with deps', async () => {
     expect(deps).toBeInstanceOf(Object);
     expect(deps.app).toBeInstanceOf(Object);
+    expect(deps.defineDatabase).toBeInstanceOf(Function);
+  });
+
+  it('define database should return instances of ContextStrategy', async () => {
+    const { mongo, postgres } = await deps.defineDatabase();
+    const { Mongo, mongoStrategy } = mongo;
+    const { Postgres, postgresStrategy, postgresConnection } = postgres;
+
+    expect(mongoStrategy).toBeInstanceOf(ContextStrategy);
+    expect(postgresStrategy).toBeInstanceOf(ContextStrategy);
+
+    Mongo.disconnect();
+    Postgres.disconnect(postgresConnection);
   });
 });
