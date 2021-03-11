1ead196df8d42428bbe8d08f85358949f6c0cc84
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-11 09:42:58 -0300

    feat(server): tests

--- src/routes/HeroRoutes/index.js
+++ src/routes/HeroRoutes/index.js
@@ -1,10 +1,21 @@
+const HeroRoutesController = require('./HeroRoutesController');
+const ContextStrategy = require('../../db/strategies/context/ContextStrategy');
+const MongoDB = require('../../db/strategies/mongodb/mongodb');
+const mongoHeroesSchema = require('../../db/strategies/mongodb/schemas/heroesSchema');
+
 const { Router } = require('express');
 const heroesRouter = Router();
 
-const listHeroes = (req, res) => {
-  res.json({ message: 'hello world' });
-};
+try {
+  const mongoConnection = MongoDB.connect();
+  const mongoDBInstance = new MongoDB(mongoConnection, mongoHeroesSchema);
+  const mongoStrategy = new ContextStrategy(mongoDBInstance);
 
-heroesRouter.get('/', listHeroes);
+  const heroRoutesController = new HeroRoutesController(mongoStrategy);
+
+  heroesRouter.get('/', heroRoutesController.list.bind(heroRoutesController));
+} catch (err) {
+  throw new Error(err);
+}
 
 module.exports = heroesRouter;
--- src/routes/HeroRoutes/index.spec.js
+++ src/routes/HeroRoutes/index.spec.js
@@ -2,8 +2,15 @@ const request = require('supertest');
 const app = require('../');
 
 describe('Test hero routes', () => {
-  it('should return a message', async () => {
-    const response = await request(app).get('/heroes');
-    expect(response.body.message).toBe('hello world');
+  describe('LIST | GET', () => {
+    it('Should return 200 status if list successfuly', async () => {
+      const response = await request(app).get('/heroes');
+      expect(response.status).toBe(200);
+    });
+
+    it('Should return an array if list successfuly', async () => {
+      const response = await request(app).get('/heroes');
+      expect(response.body).toBeInstanceOf(Array);
+    });
   });
 });
