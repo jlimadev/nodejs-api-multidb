97f0feec27d70cda99e67bf4d9977a21dc17a64c
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-11 11:27:15 -0300

    feat(server): tests to list hero

--- src/routes/HeroRoutes/index.spec.js
+++ src/routes/HeroRoutes/index.spec.js
@@ -1,10 +1,18 @@
 const request = require('supertest');
 const { app, mongoConnection } = require('../');
 
+let testId = '';
+
+const mockInsertHero = {
+  name: 'any name',
+  power: 'any power',
+};
+
 describe('Test hero routes', () => {
   afterAll(async () => {
     await mongoConnection.close();
   });
+
   describe('LIST | GET', () => {
     it('Should return 200 status if list successfuly', async () => {
       const response = await request(app).get('/heroes');
@@ -14,6 +22,22 @@ describe('Test hero routes', () => {
     it('Should return an array if list successfuly', async () => {
       const response = await request(app).get('/heroes');
       expect(response.body).toBeInstanceOf(Array);
+    });
+
+    it('Should list using query', async () => {
+      const LIMIT = 10;
+      const SKIP = 0;
+      const NAME = 'any';
+
+      const response = await request(app)
+        .get('/heroes')
+        .query({ limit: LIMIT, skip: SKIP, name: NAME });
+
+      const [firstHero] = response.body;
+      const listedHero = { name: firstHero.name, power: firstHero.power };
+
+      expect(response.body).toBeInstanceOf(Array);
+      expect(listedHero).toEqual(mockInsertHero);
     });
   });
 });
