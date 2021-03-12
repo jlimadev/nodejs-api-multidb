ced4aea339f65cbcfc233fef5a429a01210b3515
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-12 16:41:35 -0300

    refactor(folders): rename folders and files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const { errors } = require('celebrate');
 const cors = require('cors');
-
-const heroRoutes = require('./hero-routes');
-const heroRoutesValidations = require('../utils/celebrate-validations/heroRoutesValidations');
-
-// Database strategy
+const { errors } = require('celebrate');
 const ContextStrategy = require('../db/strategies/context/ContextStrategy');
+const MongoDB = require('../db/strategies/mongodb/mongodb');
+const heroRoutesValidations = require('../utils/celebrate-validations/heroRoutesValidations');
+const heroRoutes = require('./hero-routes');
 
 // MongoDB strategy and instances
-const MongoDB = require('../db/strategies/mongodb/mongodb');
 const mongoHeroesSchema = require('../db/strategies/mongodb/schemas/heroesSchema');
 const mongoConnection = MongoDB.connect();
 const mongoDBInstance = new MongoDB(mongoConnection, mongoHeroesSchema);
@@ -23,9 +20,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/heroes', heroRoutes(heroRoutesController, heroRoutesValidations));
-
 app.use(errors());
 
 module.exports = { app, mongoConnection };
