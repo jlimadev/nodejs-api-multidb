ab224c8b85258161780d7227203a903600845077
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-12 09:34:09 -0300

    feat(server): test list success cases

--- src/routes/HeroRoutes/index.spec.js
+++ src/routes/HeroRoutes/index.spec.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const { app, mongoConnection } = require('../');
-
 let testId = '';
 
 const mockInsertHero = {
@@ -31,30 +30,32 @@ describe('Test hero routes', () => {
   });
 
   describe('LIST | GET', () => {
-    it('Should return 200 status if list successfuly', async () => {
-      const response = await request(app).get('/heroes');
-      expect(response.status).toBe(200);
-    });
+    describe('Success cases', () => {
+      it('Should return 200 status if list successfuly', async () => {
+        const response = await request(app).get('/heroes');
+        expect(response.status).toBe(200);
+      });
 
-    it('Should return an array if list successfuly', async () => {
-      const response = await request(app).get('/heroes');
-      expect(response.body).toBeInstanceOf(Array);
-    });
+      it('Should return an array if list successfuly', async () => {
+        const response = await request(app).get('/heroes');
+        expect(response.body).toBeInstanceOf(Array);
+      });
 
-    it('Should list using query', async () => {
-      const LIMIT = 10;
-      const SKIP = 0;
-      const NAME = 'any';
+      it('Should list successfuly using correct query parameters', async () => {
+        const LIMIT = 10;
+        const SKIP = 0;
+        const NAME = 'any';
 
-      const response = await request(app)
-        .get('/heroes')
-        .query({ limit: LIMIT, skip: SKIP, name: NAME });
+        const response = await request(app)
+          .get('/heroes')
+          .query({ limit: LIMIT, skip: SKIP, name: NAME });
 
-      const [firstHero] = response.body;
-      const listedHero = { name: firstHero.name, power: firstHero.power };
+        const [firstHero] = response.body;
+        const listedHero = { name: firstHero.name, power: firstHero.power };
 
-      expect(response.body).toBeInstanceOf(Array);
-      expect(listedHero).toEqual(mockInsertHero);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(listedHero).toEqual(mockInsertHero);
+      });
     });
   });
 
