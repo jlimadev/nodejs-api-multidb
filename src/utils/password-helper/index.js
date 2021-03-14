b68d5aa2ba6e3a6321665baf87d424190019c540
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-14 14:30:50 -0300

    feat(server): password helper implemented

---
+++ src/utils/password-helper/index.js
@@ -0,0 +1,14 @@
+const { hash, compare } = require('bcrypt');
+const { promisify } = require('util');
+
+const asyncHash = promisify(hash);
+const asyncCompare = promisify(compare);
+
+const SALT = parseInt(process.env.PASSWORD_SALT);
+
+const hashPassword = (password) => asyncHash(password, SALT);
+
+const comparePassword = (password, hashedPassword) =>
+  asyncCompare(password, hashedPassword);
+
+module.exports = { hashPassword, comparePassword };
---
+++ src/utils/password-helper/index.spec.js
@@ -0,0 +1,26 @@
+process.env.PASSWORD_SALT = 6;
+const { hashPassword, comparePassword } = require('.');
+
+let generatedHash = '';
+
+describe('Password Helper test suit', () => {
+  describe('hashPassword test', () => {
+    it('should hash the password', async () => {
+      const result = await hashPassword('anypassword');
+      generatedHash = result;
+      expect(result.length).toBeGreaterThan(10);
+    });
+  });
+
+  describe('comparePassword test', () => {
+    it('should return true when compare the correct password', async () => {
+      const result = await comparePassword('anypassword', generatedHash);
+      expect(result).toBe(true);
+    });
+
+    it('should return false when compare the incorrect password', async () => {
+      const result = await comparePassword('anypassword', generatedHash);
+      expect(result).toBe(true);
+    });
+  });
+});
