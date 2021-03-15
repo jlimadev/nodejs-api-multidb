1caa3a28a75dc8b2865a2049cf3ac033e2b5f031
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-15 12:41:33 -0300

    feat(auth): create mongo schema

--- src/db/strategies/mongodb/schemas/authSchema.js
+++ src/db/strategies/mongodb/schemas/authSchema.js
@@ -3,11 +3,14 @@ const { v4 } = require('uuid');
 
 const modelName = 'auth';
 
-const authSchema = new Mongoose.Schema({
-  _id: { type: String, required: true, default: v4 },
-  username: { type: String, required: true, unique: true, dropDups: true },
-  password: { type: String, required: true },
-});
+const authSchema = new Mongoose.Schema(
+  {
+    _id: { type: String, required: true, default: v4 },
+    username: { type: String, required: true, unique: true, dropDups: true },
+    password: { type: String, required: true },
+  },
+  { strict: false },
+);
 
 const authModel = Mongoose.models[modelName]
   ? Mongoose.model(modelName)
