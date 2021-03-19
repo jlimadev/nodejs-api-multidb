51fccc25382b374d94bf7ff33c68dc9df57ac564
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-03-19 19:28:47 -0300

    feat(auth): test signup success

--- src/routes/auth-routes/index.spec.js
+++ src/routes/auth-routes/index.spec.js
@@ -21,9 +21,31 @@ const makeSut = () => {
     celebrateValidateSignUp: mockCallback,
   });
 
+  const defaultUser = {
+    username: 'anyusername',
+    password: 'anypassword',
+  };
+
+  const userToSignUp = {
+    ...defaultUser,
+    passwordConfirmation: 'anypassword',
+  };
+
+  const createdUser = {
+    _id: 'anyid',
+    username: 'anyusername',
+  };
+
   const sut = authRoutes;
 
-  return { sut, mockedAuthRoute, mockedValidations };
+  return {
+    sut,
+    mockedAuthRoute,
+    mockedValidations,
+    defaultUser,
+    userToSignUp,
+    createdUser,
+  };
 };
 
 describe('heroRoutes test suit', () => {
@@ -64,6 +86,19 @@ describe('heroRoutes test suit', () => {
       };
 
       expect(act).toThrow();
+    });
+  });
+
+  describe('test /auth/signup route', () => {
+    it('Should return Status 200 - [Success] when signup successfuly', async () => {
+      const { userToSignUp } = makeSut();
+
+      const response = await request(app)
+        .post('/auth/signup')
+        .send(userToSignUp);
+
+      expect(response.body.username).toBe(userToSignUp.username);
+      expect(response.status).toBe(200);
     });
   });
 });
